b7471fb44bf3846f17190ddd505e4dda8bad2157
Kabilan-16 <kabik5095@gmail.com>
2024-09-29 15:12:02 +0530

    firebase working is done

--- foodi-client/src/firebase/firebase.config.js
+++ foodi-client/src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtXmmgRb9mG8v71vyecJ-CUTN97rN2LVQ",
-  authDomain: "foodi-client-part-8a6cd.firebaseapp.com",
-  projectId: "foodi-client-part-8a6cd",
-  storageBucket: "foodi-client-part-8a6cd.appspot.com",
-  messagingSenderId: "291969286288",
-  appId: "1:291969286288:web:b3443689465d7a24189815"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
